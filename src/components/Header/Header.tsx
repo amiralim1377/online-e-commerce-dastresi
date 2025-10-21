f92f9deef29faa217df2dd122681f5432418a2a0
amirali <alimoradi7718@gmail.com>
2025-10-21 19:05:38 +0330

    feat: make header sticky with top positioning and shadow

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,8 @@ import HeaderTop from "../HeaderTop";
 
 export default function Header() {
   return (
-    <header className="h-[150px]  flex items-center justify-between    border-b w-full  ">
-      <section className="w-full custom-container   mx-auto my-8  flex flex-col gap-2">
+    <header className="sticky top-0 z-50 bg-white shadow-sm h-[150px] flex items-center justify-between border-b w-full">
+      <section className="w-full custom-container mx-auto my-8 flex flex-col gap-2">
         <HeaderTop />
         <HeaderNavigationMenu />
       </section>
